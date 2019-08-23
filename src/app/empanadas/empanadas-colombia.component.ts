6cef84b0fc9833a1e60682b643b2e327ac72f53f
PepeAndVit <49903460+PepeAndVit@users.noreply.github.com>
2019-08-23 20:10:45 +0200

    Zoom en imagenes

--- src/app/empanadas/empanadas-colombia.component.ts
+++ src/app/empanadas/empanadas-colombia.component.ts
@@ -1,11 +1,233 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-empanadas-colombia',
   template: `
-    <p>
-      empanadas-colombia works!
-    </p>
+  <div class="row">
+  <div class="col-md-12">
+
+    <div id="mdb-lightbox-ui"></div>
+
+    <div class="mdb-lightbox">
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(145).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(145).jpg" alt="placeholder"
+            class="img-fluid">
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(150).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(150).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(152).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(152).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(42).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(42).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(151).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(151).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(40).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(40).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(148).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(148).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(147).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(149).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(149).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+    </div>
+
+  </div>
+</div>
+
+  <div class="container">
+
+    <div class="row">
+        <div class="col-md-6">
+
+<!-- Card Narrower -->
+<div class="card card-cascade narrower">
+
+<!-- Card image -->
+<div class="view view-cascade overlay">
+  <img  class="card-img-top" src="../../assets/img/img-empanadas/EMPANADA NO SE 1.JPG" alt="Card image cap">
+  <a>
+    <div class="mask rgba-white-slight"></div>
+  </a>
+</div>
+
+<!-- Card content -->
+<div class="card-body card-body-cascade">
+
+  <!-- Label -->
+  <h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i>Empanadas</h5>
+  <!-- Title -->
+  <h4 class="font-weight-bold card-title">De Pollo</h4>
+</div>
+
+
+
+</div>
+<!-- Card Narrower -->
+
+
+
+  </div>
+  <div class="col-md-6">
+<!-- Card Narrower -->
+<div class="card card-cascade narrower">
+
+<!-- Card image -->
+<div class="view view-cascade overlay">
+  <img  class="card-img-top" src="../../assets/img/img-empanadas/IMG_4103.JPG" alt="Card image cap">
+  <a>
+    <div class="mask rgba-white-slight"></div>
+  </a>
+</div>
+
+<!-- Card content -->
+<div class="card-body card-body-cascade">
+
+  <!-- Label -->
+  <h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Empanadas</h5>
+  <!-- Title -->
+  <h4 class="font-weight-bold card-title">Carne Picada</h4>
+  <!-- Text -->
+  <p class="card-text">Empanadas de maiz, echas de carne picada, muchas cosas más...</p>
+
+</div>
+
+</div>
+<!-- Card Narrower -->
+        </div>
+    </div>
+
+  </div>
+<br>
+<div class="container">
+
+  <div class="row">
+      <div class="col-md-6">
+
+<!-- Card Narrower -->
+<div class="card card-cascade narrower">
+
+<!-- Card image -->
+<div class="view view-cascade overlay">
+<img  class="card-img-top" src="../../assets/img/img-empanadas/IMG_4098.JPG" alt="Card image cap">
+<a>
+  <div class="mask rgba-white-slight"></div>
+</a>
+</div>
+
+<!-- Card content -->
+<div class="card-body card-body-cascade">
+
+<!-- Label -->
+<h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Culinary</h5>
+<!-- Title -->
+<h4 class="font-weight-bold card-title">Cheat day inspirations</h4>
+<!-- Text -->
+<p class="card-text">Ut enim ad minima veniam, quis nostrum exercitationem
+ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.</p>
+<!-- Button -->
+
+
+</div>
+
+
+
+</div>
+<!-- Card Narrower -->
+
+
+
+
+</div>
+<div class="col-md-6">
+ <!-- Card Narrower -->
+<div class="card card-cascade narrower">
+
+<!-- Card image -->
+<div class="view view-cascade overlay">
+<div class="mdb-lightbox">
+<a src="../../assets/img/img-empanadas/EMPANADAS 222.JPG" data-size="1600x1067">
+          <img src="../../assets/img/img-empanadas/EMPANADAS 222.JPG" alt="placeholder"
+            class="img-fluid" />
+        </a>
+</div>
+
+<a>
+  <div class="mask rgba-white-slight"></div>
+</a>
+</div>
+
+<!-- Card content -->
+<div class="card-body card-body-cascade">
+
+<!-- Label -->
+<h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Culinary</h5>
+<!-- Title -->
+<h4 class="font-weight-bold card-title">Cheat day inspirations</h4>
+<!-- Text -->
+<p class="card-text">Ut enim ad minima veniam, quis nostrum exercitationem
+ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.</p>
+
+</div>
+
+
+</div>
+<!-- Card Narrower -->
+      </div>
+  </div>
+
+</div>
+
+<script type="text/javascript">
+$(function () {
+ $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+</script>
+
   `,
   styles: []
 })
--- src/app/empanadas/empanadas-ecuador.component.ts
+++ src/app/empanadas/empanadas-ecuador.component.ts
@@ -3,7 +3,86 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empanadas-ecuador',
   template: `
-    <img src="../../assets/img/bandera ecuador.png">
+  <div class="row">
+  <div class="col-md-12">
+
+    <div id="mdb-lightbox-ui"></div>
+    
+
+    <div class="mdb-lightbox">
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(145).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(145).jpg" alt="placeholder"
+            class="img-fluid">
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(150).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(150).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(152).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(152).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(42).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(42).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(151).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(151).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(40).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(40).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(148).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(148).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(147).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+
+      <figure class="col-md-4">
+        <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(149).jpg" data-size="1600x1067">
+          <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(149).jpg" alt="placeholder"
+            class="img-fluid" />
+        </a>
+      </figure>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+$(function () {
+ $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+</script>
+
   `,
   styles: []
 })
