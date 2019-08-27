0e4515fb9d1624989b28124a6bf28f558192d51c
djkluisA <djkluis@gmail.com>
2019-08-27 16:03:21 +0200

    carrusel

--- src/app/empanadas/empanadas-mundo.component.ts
+++ src/app/empanadas/empanadas-mundo.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="./../assets/img/globo latinoamerincano.png" alt="First slide">
+      <img class="d-block w-100" src="./../assets/img/img-empanadas/EMPANADA NO SE 1.JPG" alt="First slide">
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="./../assets/img/globo latinoamerincano.png" alt="Second slide">
