23c9621dfe9b97b89b2fb7ee9280a41be7843fec
djkluisA <djkluis@gmail.com>
2019-08-27 16:47:22 +0200

    carusel

--- src/app/empanadas/empanadas-mundo.component.ts
+++ src/app/empanadas/empanadas-mundo.component.ts
@@ -3,16 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empanadas-mundo',
   template: `
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="row">
+      <div class="col-md-12">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="./../assets/img/img-empanadas/EMPANADA NO SE 1.JPG" alt="First slide">
+      <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADA NO SE 1.JPG" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="./../assets/img/globo latinoamerincano.png" alt="Second slide">
+      <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADAS 222.JPG" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="./../assets/img/globo latinoamerincano.png" alt="Third slide">
+      <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADAS 222.JPG" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../../assets/img/img-empanadas/IMG_4098.JPG" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../../assets/img/img-empanadas/IMG_4103.JPG" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -23,7 +31,34 @@ import { Component, OnInit } from '@angular/core';
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>  `,
+</div>
+      </div>
+      <br>
+      <div class="col-md-12">
+      <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+  <div class="carousel-item active">
+  <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADA NO SE 1.JPG" alt="First slide">
+</div>
+<div class="carousel-item">
+  <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADAS 222.JPG" alt="Second slide">
+</div>
+<div class="carousel-item">
+  <img class="d-block w-100" src="../../assets/img/img-empanadas/IMG_4098.JPG" alt="Second slide">
+</div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+      </div>
+    </div>
+   `,
 
   styles: []
 })
