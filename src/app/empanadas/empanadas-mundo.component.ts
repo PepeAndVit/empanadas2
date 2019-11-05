b7c56cd1583f815ef82f5fff6848d72d312233bf
Luis Antonio Saavedra Palacios <djkluis@gmail.com>
2019-11-05 07:45:51 +0100

    limpiar
    limpiar

--- src/app/empanadas/empanadas-mundo.component.ts
+++ src/app/empanadas/empanadas-mundo.component.ts
@@ -9,18 +9,38 @@ import { Component, OnInit } from '@angular/core';
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADA NO SE 1.JPG" alt="First slide">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADAS 222.JPG" alt="Second slide">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADAS 222.JPG" alt="Third slide">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="../../assets/img/img-empanadas/IMG_4098.JPG" alt="Second slide">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="../../assets/img/img-empanadas/IMG_4103.JPG" alt="Third slide">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -33,32 +53,9 @@ import { Component, OnInit } from '@angular/core';
   </a>
 </div>
       </div>
-  <br>
-      <div class="col-md-12">
-      <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-  <div class="carousel-item active">
-  <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADA NO SE 1.JPG" alt="First slide">
-</div>
-<div class="carousel-item">
-  <img class="d-block w-100" src="../../assets/img/img-empanadas/EMPANADAS 222.JPG" alt="Second slide">
-</div>
-<div class="carousel-item">
-  <img class="d-block w-100" src="../../assets/img/img-empanadas/IMG_4098.JPG" alt="Second slide">
-</div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-      </div>
+ 
     </div>
-   `,
+    `,
 
   styles: []
 })
